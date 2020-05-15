337c45737fb05458799452fa42f45758c3587925
alita-99 <alita-99>
2020-05-14 20:36:56 -0700

    Launcher with an example which draws a line

---
+++ launcher.java
@@ -0,0 +1,42 @@
+/**
+ * This launcher creates a window with canvas and then
+ * calls draw method passing GraphicsContext to it.
+ *
+ * To use this launcher:
+ * - define draw method
+ * - /open this launcher at the end of your script
+ *
+ */
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.canvas.*;
+ 
+public class Launcher extends Application 
+{
+    public static void main(String[] args) 
+    {
+        launch(args);
+    }
+ 
+    public void start(Stage stage) 
+    {
+        Group root = new Group();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+
+        Canvas canvas = new Canvas(600, 400);
+        root.getChildren().add(canvas);
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.setLineWidth(2);
+        draw(gc);
+
+        stage.show();
+    }
+}
+
+// runs the application
+Launcher.main(null);
+
---
+++ line.java
@@ -0,0 +1,5 @@
+void draw(GraphicsContext gc) {
+    gc.strokeLine(40, 10, 10, 40);
+}
+
+/open launcher.java
