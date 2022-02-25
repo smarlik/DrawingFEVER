ae8677c2f7376843424a6de4f4df29af52cfa369
smarlik <smarlik@protonmail.com>
2022-02-24 17:55:13 -0800

    I editied this program so you can change the size of the smiles and a smile rain

--- libgdx/smile.java
+++ libgdx/smile.java
@@ -1,24 +1,31 @@
 // This program draws a smile. I got the idea for this program from the new edition of beanz!S
 
 void draw(ShapeRenderer shapeRenderer) {
-    //shapeRenderer.begin(ShapeType.Filled);
-    //shapeRenderer.setColor(Color.PINK);
-    //shapeRenderer.line(85,80,85,200);
-    //shapeRenderer.end();
+
+	int y= rand(300);
+	int x= rand(500);
+	int r= rand(50);
+	drawsmile (shapeRenderer,r,x,y);
+	sleep(250);
+}
+
+void drawsmile(ShapeRenderer shapeRenderer ,int r, int x, int y) {
 	
     shapeRenderer.begin(ShapeType.Filled);
     shapeRenderer.setColor(Color.YELLOW);
-    shapeRenderer.circle(400,300,150);
+    shapeRenderer.circle(x,y,r);
+	
+	// ed= eye distance
+	int ed= r/10 * 7;
 	
 	shapeRenderer.setColor(Color.BLACK);
-    shapeRenderer.circle(330,300,25);
+    shapeRenderer.circle(x-ed/2,y+r*0.3F,r/8);
 	
 	shapeRenderer.setColor(Color.BLACK);
-    shapeRenderer.circle(470,300,25);
+    shapeRenderer.circle(x+ed/2,y+r*0.3F,r/8);
 	
 	shapeRenderer.setColor(Color.RED);
-	shapeRenderer.arc(400, 250, 75, 180, 180);
-	
+	shapeRenderer.arc(x*0.999F, y+r*0.01F, r*0.9F, 180, 180);
 	
 	shapeRenderer.end();
 
